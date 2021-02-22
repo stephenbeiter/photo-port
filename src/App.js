eaa30d947fa9dd82c839dc48fb80e00c9965c76a
Stephen Beiter <stephenbeiter@gmail.com>
2021-02-21 22:58:03 -0800

    added contact component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const categories = [
@@ -24,12 +25,11 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery
-            currentCategory={currentCategory}
-          ></Gallery>
-          <About></About>
-        </div>
+        <ContactForm></ContactForm>
+        <Gallery
+          currentCategory={currentCategory}
+        ></Gallery>
+        <About></About>
       </main>
     </div>
   );
